b33b5066bfd85ef4d08a2f8ada00bfd1d0c65a43
TeraByte900 <fe.n.teracini@hotmail.com>
2019-04-08 16:54:59 -0300

    velocidade e distancia

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -34,9 +34,12 @@
 #define BUT3_PIO_IDX       19u
 #define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
 
-volatile int radius = 0.65/2;
+volatile float radius = 0.325;
 volatile int counter = 0;
 volatile int but_flag = 0;
+volatile int dtc = 0;
+
+volatile Bool f_rtt_alarme = false;
 
 
 struct ili9488_opt_t g_ili9488_display_opt;
@@ -80,6 +83,23 @@ void BUT2_Handler(){
 void BUT3_Handler(){
 	but_flag = 0;
 	counter += 1;
+	dtc += counter;
+}
+
+void RTT_Handler(void)
+{
+	uint32_t ul_status;
+
+	/* Get RTT status */
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {  }
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {// BLINK Led
+		f_rtt_alarme = true;                  // flag RTT alarme
+	}
 }
 
 void But_init(){
@@ -135,6 +155,31 @@ void RTC_init(){
 
 }
 
+static float get_time_rtt(){
+	uint ul_previous_time = rtt_read_timer_value(RTT);
+}
+
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
+{
+	uint32_t ul_previous_time;
+
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
+	
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 0);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN);
+}
+
 
 int main(void) {
 	board_init();
@@ -143,21 +188,52 @@ int main(void) {
 	But_init();
 	RTC_init();
 	
+	WDT->WDT_MR = WDT_MR_WDDIS;
+	
 	char speed_buffer[32];
 	char rotation_counter_buffer[8];
+	char distance_buffer[32];
+	
+	float distance = 0;
+	float speed = 0;
+	float w;
 	
 	font_draw_text(&calibri_36, "BIKE", 120, 50, 1);
 	font_draw_text(&calibri_36, "VELOCIDADE", 50, 100, 1);
+	sprintf(speed_buffer, "%f", speed);
+	font_draw_text(&calibri_36, speed_buffer, 50, 150, 1);
 	font_draw_text(&calibri_36, "DISTANCIA", 50, 200, 1);
+	sprintf(distance_buffer, "%f", distance);
+	font_draw_text(&calibri_36, distance_buffer, 50, 250, 1);
 	font_draw_text(&calibri_36, "TEMPO", 50, 300, 1);
 	
 	sprintf(rotation_counter_buffer, "%d", counter);
 	font_draw_text(&calibri_36, rotation_counter_buffer, 50, 400, 1);
+	
+	f_rtt_alarme = true;
 	while(1) {
 		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
 		if(but_flag == 0){
 			sprintf(rotation_counter_buffer, "%d", counter);
 			font_draw_text(&calibri_36, rotation_counter_buffer, 50, 400, 1);
+		}
+		if (f_rtt_alarme){
+			uint16_t pllPreScale = (int) (((float) 32768) / 1);
+			uint32_t irqRTTvalue  = 4;
+			RTT_init(pllPreScale, irqRTTvalue);
+			
+			distance = 2*3.14*radius*counter;
+			
+			w = 2*3.14*dtc/4;
+			dtc = 0;
+			speed = w*radius/3.6;
+			
+			sprintf(speed_buffer, "%f", speed);
+			font_draw_text(&calibri_36, speed_buffer, 50, 150, 1);         
+      	
+			sprintf(distance_buffer, "%f", distance);
+			font_draw_text(&calibri_36, distance_buffer, 50, 250, 1);
+			f_rtt_alarme = false;
 		}
 	}
 }
